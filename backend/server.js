ba3126e9fd8f3d92c179dea5fde055eacbdf0abf
Osman Hersi <osmanhersi@hotmail.co.uk>
2026-03-05 11:41:51 +0000

    finished my post route for tickets and got it working with a 201 response

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
-const pool = require('./db.js');
+const pool = require("./db.js");
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post("/api/tickets", async (req, res) => {
+  try {
+    const { title, description, priority } = req.body;
+    if (!title || !description) {
+      return res.status(400).json({
+        error: "Please enter a title AND description for your ticket!",
+      });
+    }
+    const validatedPriority = priority || "MEDIUM";
+    const [result] = await pool.query(
+      `INSERT INTO tickets (title, description, priority) VALUES (?, ?, ?)`,
+      [title.trim(), description.trim(), validatedPriority],
+    );
+    res.status(201).json({
+      title: title.trim(),
+      description: description.trim(),
+      priority: validatedPriority,
+      status: "OPEN",
+      id: result.insertId,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.get("/test-db", async (req, res) => {
