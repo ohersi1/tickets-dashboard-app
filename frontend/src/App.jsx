6d5cca226670d368f5715a219f0a752163353d68
Osman Hersi <osmanhersi@hotmail.co.uk>
2026-03-21 01:27:40 +0000

    started working on search feature in the frontend started adding a controlled input so I can capture search value

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,52 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { use } from "react";
 
 function App() {
   const [tickets, setTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
     fetch("http://localhost:3000/api/tickets")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setTickets(data.results);
+        setLoading(false);
       })
-      .catch(err => console.error(err));
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) return <p>Loading tickets...</p>;
+  if (error) return <p>Error: {error}</p>;
   return (
     <div>
       <h1>Tickets Dashboard</h1>
-      {console.log("tickets state:", tickets)}
-      <ul>
-        {tickets.map((ticket) => (
-          <li key={ticket.id}>{ticket.title}</li>
-        ))}
-      </ul>
+      <input type="text" placeholder="Search tickets..." value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Priority</th>
+            <th>Status</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((ticket) => (
+            <tr key={ticket.id}>
+              <td>{ticket.title}</td>
+              <td>{ticket.priority}</td>
+              <td>{ticket.status}</td>
+              <td>{new Date(ticket.created_at).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
