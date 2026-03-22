601949eb036a8b9c14751172235840b3ad9da8f7
Osman Hersi <osmanhersi@hotmail.co.uk>
2026-03-22 03:25:07 +0000

    still working on search input fixing cascading rendering issue in useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { use } from "react";
 
 function App() {
   const [tickets, setTickets] = useState([]);
@@ -8,8 +7,11 @@ function App() {
   const [error, setError] = useState(null);
   const [searchValue, setSearchValue] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:3000/api/tickets")
+  const fetchTickets = () => {
+    setLoading(true);
+  setError(null);
+  
+     fetch("http://localhost:3000/api/tickets")
       .then((res) => res.json())
       .then((data) => {
         setTickets(data.results);
@@ -19,14 +21,49 @@ function App() {
         setError(err.message);
         setLoading(false);
       });
+  }
+
+  useEffect(() => {
+   fetchTickets();
   }, []);
+
+  const handleSearch = () => {
+    if (!searchValue.trim()) return;
+    setLoading(true);
+    setError(null);
+    fetch(
+      `http://localhost:3000/api/tickets?search=${encodeURIComponent(searchValue)}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setTickets(data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  };
+
+  const handleReset = () => {
+    setSearchValue("");
+    setError(null);
+    fetchTickets()
+  }
 
   if (loading) return <p>Loading tickets...</p>;
   if (error) return <p>Error: {error}</p>;
   return (
     <div>
       <h1>Tickets Dashboard</h1>
-      <input type="text" placeholder="Search tickets..." value={searchValue} onChange={e => setSearchValue(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Search tickets..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleReset}>Reset</button>
       <table>
         <thead>
           <tr>
